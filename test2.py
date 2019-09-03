adc2e9f1ddf608758d7a5e70b9720619386695a5
ebroadbent <broadbent52@aol.com>
2019-09-03 14:11:33 -0600

    finished mathlab triangles

---
+++ test2.py
@@ -0,0 +1,30 @@
+# Eric Broadbent
+# 9/19
+# Math Lab - Triangles
+
+# creat and asigne values to base and height
+base = 10
+height = 5.0
+
+# calculate area
+area = 0.5*base*height
+
+print(area)
+area_type = type(area)
+print(area_type)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
